3e69aa4b9a241e21a6b3afa87fbc34c112117598
Ader Barbosa <119372914+AderBarbosa@users.noreply.github.com>
2022-12-03 00:42:54 +0000

    Finalizado

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string entrada;
+    double fahren, celsius; 
+  Console.WriteLine ("Escreva a Temperatura em Fahrenheit ");
+  entrada = Console.ReadLine();
+  fahren = Double.Parse(entrada);
+  celsius = (fahren - 32) * (5.0/9.0);
+    Console.WriteLine ("A temperatura em Graus Celsius é de: " + celsius);
+  }
+}
